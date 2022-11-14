b4ca022751823bc91ffef10b738596e1a496c888
ethanqm <q.ethan.moore@gmail.com>
2022-11-14 14:44:44 +1100

    first successful run

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
         io::Write, 
         process::Command
 };
-
+use regex::Regex;
 
 mod time;
 mod metadata;
@@ -12,7 +12,13 @@ mod parse;
 
 
 fn main() {
+    let header = ";FFMETADATA1\n";
     let args: Vec<String> = env::args().collect();
+    let video_path = &args[2];
+
+    let re = Regex::new(r"^.*/(.*)").unwrap();
+
+    let video_name = re.captures(video_path).expect("could not decipher video file name").get(1).unwrap().as_str();
 
 
     let file = fs::read_to_string(&args[1]).expect("Where da timestamp file at?");
@@ -28,17 +34,23 @@ fn main() {
         }
     }
 
+    // read arg[1] and write to output.txt
+    let mut outf = File::create("output.txt").expect("failed to create output file");
+    let meta_out = format!("{}{}", header, chaps.iter().map(metadata::Chapter::output).collect::<String>());
+    write!(outf, "{}",meta_out).expect("failed to write to output file");
+
     //ffmpeg extract video metadata
     Command::new("ffmpeg").arg("-i")
-                                .arg(&args[2]) //video file path
-                                .arg("-f")
-                                .arg("ffmetadata")
-                                .arg("metadata.txt")//@TODO: make unique to avoid clash
-                                .output()
+                                .arg(video_path)
+                                .arg("-i")
+                                .arg("output.txt")
+                                .arg("-map_metadata")//@TODO: make unique to avoid clash
+                                .arg("0")
+                                .arg("-codec")
+                                .arg("copy")
+                                .arg(format!("{}{}","STAMPED",video_name))
+                                .status()
                                 .expect("Failed to extract video metadata with ffmpeg");
 
-    // read arg[1] and write to output.txt
-    // let mut output = File::create("output.txt").expect("failed to create output file");
-    // write!(output, "{}", chaps.iter().map(metadata::Chapter::output).collect::<String>()).expect("failed to write to output file");
 
 }
