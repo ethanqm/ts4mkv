033b15b38b9e59ef33f637129c41dcc694b5bbee
ethanqm <q.ethan.moore@gmail.com>
2022-11-15 15:10:52 +1100

    formatting and move to tabs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{
-        fs::{File, self}, 
-        env, 
-        io::Write, 
-        process::Command
+		fs::{File, self}, 
+		env, 
+		io::Write, 
+		process::Command
 };
 use regex::Regex;
 
@@ -12,44 +12,44 @@ mod parse;
 
 
 fn main() {
-    let header = ";FFMETADATA1\n";//basically overrides the metadata tho
-    let args: Vec<String> = env::args().collect();
-    let video_path = &args[2];
+	let header = ";FFMETADATA1\n";//basically overrides the metadata tho
+	let args: Vec<String> = env::args().collect();
+	let video_path = &args[2];
 
-    let re = Regex::new(r"^[./]*(.*)").unwrap();
-    let video_name = re.captures(video_path).expect("could not decipher video file name").get(1).unwrap().as_str();
+	let re = Regex::new(r"^[./]*(.*)").unwrap();
+	let video_name = re.captures(video_path).expect("could not decipher video file name").get(1).unwrap().as_str();
 
 
-    let file = fs::read_to_string(&args[1]).expect("Where da timestamp file at?");
-    let mut chaps: Vec<metadata::Chapter> = file.lines()
-        .map(parse::parse_line)
-        .collect();
-    
-    //set end times
-    for i in 0..chaps.len() {
-        match chaps.get(i+1) {
-            Some(next_chap) => chaps[i].end = next_chap.start,
-            None => chaps[i].end = u32::MAX,
-        }
-    }
+	let file = fs::read_to_string(&args[1]).expect("Where da timestamp file at?");
+	let mut chaps: Vec<metadata::Chapter> = file.lines()
+		.map(parse::parse_line)
+		.collect();
+	
+	//set end times
+	for i in 0..chaps.len() {
+		match chaps.get(i+1) {
+			Some(next_chap) => chaps[i].end = next_chap.start,
+			None => chaps[i].end = u32::MAX,
+		}
+	}
 
-    // read arg[1] and write to output.txt
-    let mut outf = File::create("output.txt").expect("failed to create output file");
-    let meta_out = format!("{}{}", header, chaps.iter().map(metadata::Chapter::output).collect::<String>());
-    write!(outf, "{}",meta_out).expect("failed to write to output file");
+	// read arg[1] and write to output.txt
+	let mut outf = File::create("output.txt").expect("failed to create output file");
+	let meta_out = format!("{}{}", header, chaps.iter().map(metadata::Chapter::output).collect::<String>());
+	write!(outf, "{}",meta_out).expect("failed to write to output file");
 
-    //ffmpeg extract video metadata
-    Command::new("ffmpeg").arg("-i")
-                                .arg(video_path)
-                                .arg("-i")
-                                .arg("output.txt") //should maybe be unique
-                                .arg("-map_metadata")
-                                .arg("0")
-                                .arg("-codec")
-                                .arg("copy")
-                                .arg(format!("{}{}","STAMPED",video_name))
-                                .status()
-                                .expect("Failed to extract video metadata with ffmpeg");
+	//ffmpeg extract video metadata
+	Command::new("ffmpeg").arg("-i")
+						  .arg(video_path)
+						  .arg("-i")
+						  .arg("output.txt") //should maybe be unique
+						  .arg("-map_metadata")
+						  .arg("0")
+						  .arg("-codec")
+						  .arg("copy")
+						  .arg(format!("{}{}","STAMPED",video_name))
+						  .status()
+						  .expect("Failed to extract video metadata with ffmpeg");
 
 
 }
